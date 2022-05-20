f0d6991c69f0268098dc6e6d9950ecfb585caed0
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-05-20 13:16:25 +0300

    Added Comments

--- game.js
+++ game.js
@@ -1,14 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(){
+
+    // Getting elements from the HTML file
     var h2 = document.getElementById('status');
     var start = document.getElementById('start');
     var end = document.getElementById('end');
     var boundary = document.getElementsByClassName('boundary');
     var example = document.getElementsByClassName('example');
     var game = document.getElementById('game');
-    var red_bounds = false;
-    var lost_state = false
-    var score = 0;
+
+    // For when boundaries are red
+    var red_bounds = false; 
+
+    // A state to keep track if the user has lost
+    var lost_state = false;
+
+    // Initializing the score
+    var score = 0; 
     
+    // On startup code + Events
     h2.innerText += "\nClick on the Score to reset the score to 0."
     start.onclick = reset;
     end.onmouseover = winning;
@@ -16,10 +25,12 @@ document.addEventListener("DOMContentLoaded", function(){
     example[0].onclick = restart;
     game.onmouseleave = losing;
 
+    // If the mouse is hovered over the boundaries
     for (var i = 0; i < boundary.length - 1; i++){
         boundary[i].onmouseover = losing;
     }
 
+    // Function for if the mouse got over or inside the boundaries
     function losing(){
         if (red_bounds === false && lost_state === false){
             for (var i = 0; i < boundary.length; i++){
@@ -33,6 +44,7 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     }
 
+    // Function to reset the game but keep the score
     function reset(){
         for (var i = 0; i < boundary.length; i++){
             boundary[i].style.backgroundColor = '#eeeeee';
@@ -43,6 +55,7 @@ document.addEventListener("DOMContentLoaded", function(){
         lost_state = false;
     }
 
+    // Function for when the mouse is hovered over the end
     function winning(){
         if (red_bounds === false && lost_state === false) {
             h2.innerText = "You won! Click on 'S' to start again.";
@@ -51,6 +64,7 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     }
 
+    // Calls the score variable to increase or decrease
     function score_call(){
         if (red_bounds === true && lost_state === true){
             score -= 10;
@@ -67,12 +81,14 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     }
 
+    // Function for when the user clicks on score it resets the score to 0
     function restart(){
         score = 0;
         updateScore();
         reset();
     }
 
+    // Function to update the displayed score everytime an event occurs
     function updateScore(){
         example[0].innerHTML = `Score = ${score}`;
     }
